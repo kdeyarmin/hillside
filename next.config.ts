df69165d59e450f53b8610555cdbd90b81dd69df
Kevin Deyarmin <kdeyarmin@comcast.net>
2026-08-08 21:06:28 -0400

    Add Next.js config

---
+++ next.config.ts
@@ -0,0 +1,13 @@
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  output: 'standalone',
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'images.unsplash.com' },
+      { protocol: 'https', hostname: 'm.media-amazon.com' }
+    ]
+  }
+};
+
+export default nextConfig;
